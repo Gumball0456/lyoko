7c0142ed0c664dfd25268751bedee9526d1db0f3
Darilton Mario <dariltonmario14@gmail.com>
2025-05-13 18:02:38 +0000

    Code refactor

--- app/routes/home.route.js
+++ app/routes/home.route.js
@@ -1,7 +1,5 @@
 import { render } from "../controllers/home.controller.js";
 
 export function homeRoute (app){
-    app.get('/', (req,res) => {
-        render(req,res)
-    });
+    app.get('/', (req,res) => render(req,res));
 }
